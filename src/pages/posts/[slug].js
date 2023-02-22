4c2a01724ecc5a4cc3bd37baabd2c3c03ffb8d68
Marcel Milosz <marcel.milosz99@gmail.com>
2023-02-22 08:24:43 +0100

    Added progress bar for reading post

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,15 +1,53 @@
 import PostContent from "components/posts/post-detail/post-content";
 import { getPostData, getPostsFiles } from "lib/posts-util";
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+
+import classes from '../../../components/posts/slug.module.scss'
 
 function PostDetailPage(props) {
+
+    function ReadingIndicator() {
+        const [indicator, setIndicator] = useState(0);
+
+        const scroller = () => {
+            let element = document.documentElement,
+                top = element.scrollTop || document.body.scrollTop,
+                height = element.scrollHeight || document.body.scrollHeight;
+            let calculate = (top / (height - element.clientHeight)) * 100;
+            setIndicator(parseInt(calculate));
+        };
+
+        useEffect(() => {
+            window.addEventListener("scroll", scroller);
+            return () => window.removeEventListener("scroll", scroller);
+        }, [indicator]);
+
+        return (
+            <ProgressBar style={{ width: indicator + "%" }}></ProgressBar>
+        );
+    }
+
+    function ProgressBar(props) {
+
+        let width = props.style.width;
+
+        return (
+            <div className={classes.ReadProgressContainer}>
+                <div className={classes.ProgressBar} style={{ width: width }}> </div>
+            </div>
+        )
+    }
+
+
     return (
         <Fragment>
             <Head>
                 <title> {props.post.title} </title>
                 <meta name="description" content={props.post.excerpt} />
             </Head>
+
+            <ReadingIndicator />
 
             <PostContent post={props.post} />
         </Fragment>
